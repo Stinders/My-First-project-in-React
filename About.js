02bbc18a34a42092f0177f9de573c7c402e2e5b7
Satinderpal Singh <98504608+Stinders@users.noreply.github.com>
2022-01-30 22:37:46 -0500

    Add files via upload

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an About components.
+            <p>Lorem 34sg;jngvdolvnd'fbdfbnvw/lgnsglsngSnd/lkns/glkdndhgs,mnd;godjdsbg;adgns'oignsddnv;obofbnfbndbhdnbdlknsblnbdljdng'o</p>
+        </div>
+    )
+}
